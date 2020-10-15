04dc15ea6bba5da506d2f5179c5d0a09ff226c66
Falsin <greenldend@gmail.com>
2020-10-15 16:35:37 +0700

    small changes

--- script.js
+++ script.js
@@ -182,3 +182,9 @@ function removeDiv() {
         list.remove();
     }
 }
+
+window.onload = setTimeout(function() {
+    window.scrollTo(0, 1);
+})
+
+{/* <body onLoad="setTimeout(function() {window.scrollTo(0, 1)}, 100);"></body> */}
